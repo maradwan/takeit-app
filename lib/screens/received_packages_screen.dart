f29840813c0d5e2861bf66435af984be391b9a8e
amrkhaledccd <amrkhaledccd@hotmail.com>
2022-09-18 03:26:14 +0100

    show traveler requests loading indicator

--- lib/screens/received_packages_screen.dart
+++ lib/screens/received_packages_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:travel_app/model/request_status.dart';
 import 'package:travel_app/providers/traveler_requests_provider.dart';
@@ -14,19 +17,35 @@ class RecievedPackagesScreen extends StatefulWidget {
 
 class _RecievedPackagesScreenState extends State<RecievedPackagesScreen> {
   var requestStatus = RequestStatus.pending;
+  var isLoading = false;
 
   @override
   void initState() {
     Future.delayed(Duration.zero, () async {
-      _initRequests();
+      _initRequests(true);
     });
     super.initState();
   }
 
-  Future<void> _initRequests() async {
-    final requestProvider =
-        Provider.of<TravelerRequestsProvider>(context, listen: false);
-    await requestProvider.findRequests(requestStatus);
+  Future<void> _initRequests(bool showLoadingProgress) async {
+    if (showLoadingProgress) {
+      setState(() {
+        isLoading = true;
+      });
+    }
+
+    try {
+      final requestProvider =
+          Provider.of<TravelerRequestsProvider>(context, listen: false);
+      await requestProvider.findRequests(requestStatus);
+    } on HttpException catch (e) {
+      debugPrint(e.message);
+    }
+    if (showLoadingProgress) {
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   @override
@@ -39,76 +58,94 @@ class _RecievedPackagesScreenState extends State<RecievedPackagesScreen> {
         automaticallyImplyLeading: true,
       ),
       drawer: const MainDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          bottom: 0,
-          top: 10,
-          left: 10,
-          right: 10,
-        ),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FilterChip(
-                  label: const Text('Pending'),
-                  selected: requestStatus == RequestStatus.pending,
-                  onSelected: (selected) {
-                    if (requestStatus != RequestStatus.pending) {
-                      setState(() {
-                        requestStatus = RequestStatus.pending;
-                      });
-                      _initRequests();
-                    }
-                  }),
-              const SizedBox(width: 15),
-              FilterChip(
-                  label: const Text('Accepted'),
-                  selected: requestStatus == RequestStatus.accepted,
-                  onSelected: (selected) {
-                    if (requestStatus != RequestStatus.accepted) {
-                      setState(() {
-                        requestStatus = RequestStatus.accepted;
-                      });
-                      _initRequests();
-                    }
-                  }),
-              const SizedBox(width: 15),
-              FilterChip(
-                label: const Text('Declined'),
-                selected: requestStatus == RequestStatus.declined,
-                onSelected: (selected) {
-                  if (requestStatus != RequestStatus.declined) {
-                    setState(() {
-                      requestStatus = RequestStatus.declined;
-                    });
-                    _initRequests();
-                  }
-                },
-              )
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _initRequests(false),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            bottom: 0,
+            top: 10,
+            left: 10,
+            right: 10,
           ),
-          Expanded(
-            child: Consumer<TravelerRequestsProvider>(
-              builder: (ctx, requestsProvider, _) {
-                final requests = requestsProvider.requests;
-
-                return requests.isEmpty
-                    ? Center(
-                        child: Text(
-                          'You didn\'t recieve any requests yet',
-                          style: TextStyle(color: Colors.grey[600]),
-                        ),
-                      )
-                    : ListView.builder(
-                        itemCount: requests.length,
-                        itemBuilder: (ctx, i) =>
-                            TravelerRequestCard(request: requests[i]),
-                      );
-              },
+          child: Column(mainAxisSize: MainAxisSize.max, children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilterChip(
+                    label: const Text('Pending'),
+                    selected: requestStatus == RequestStatus.pending,
+                    onSelected: (selected) {
+                      if (requestStatus != RequestStatus.pending) {
+                        setState(() {
+                          requestStatus = RequestStatus.pending;
+                        });
+                        _initRequests(true);
+                      }
+                    }),
+                const SizedBox(width: 15),
+                FilterChip(
+                    label: const Text('Accepted'),
+                    selected: requestStatus == RequestStatus.accepted,
+                    onSelected: (selected) {
+                      if (requestStatus != RequestStatus.accepted) {
+                        setState(() {
+                          requestStatus = RequestStatus.accepted;
+                        });
+                        _initRequests(true);
+                      }
+                    }),
+                const SizedBox(width: 15),
+                FilterChip(
+                  label: const Text('Declined'),
+                  selected: requestStatus == RequestStatus.declined,
+                  onSelected: (selected) {
+                    if (requestStatus != RequestStatus.declined) {
+                      setState(() {
+                        requestStatus = RequestStatus.declined;
+                      });
+                      _initRequests(true);
+                    }
+                  },
+                )
+              ],
             ),
-          ),
-        ]),
+            isLoading
+                ? const Expanded(
+                    child: SizedBox(
+                      height: double.infinity,
+                      child: Center(
+                        child: SizedBox(
+                          width: 60,
+                          child: LoadingIndicator(
+                            strokeWidth: 1,
+                            indicatorType: Indicator.ballPulse,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: Consumer<TravelerRequestsProvider>(
+                      builder: (ctx, requestsProvider, _) {
+                        final requests = requestsProvider.requests;
+
+                        return requests.isEmpty
+                            ? Center(
+                                child: Text(
+                                  'You didn\'t recieve any requests yet',
+                                  style: TextStyle(color: Colors.grey[600]),
+                                ),
+                              )
+                            : ListView.builder(
+                                itemCount: requests.length,
+                                itemBuilder: (ctx, i) =>
+                                    TravelerRequestCard(request: requests[i]),
+                              );
+                      },
+                    ),
+                  ),
+          ]),
+        ),
       ),
     );
   }
